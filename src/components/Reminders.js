670f5a7ef41e779ff7bd878293a67a2f1ccb12d4
NoPref <mrrmayvk@gmail.com>
2024-11-28 01:15:10 +0300

    commented

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Reminders.css'; // External CSS file for styling
-import { requestNotificationPermission } from '../firebase-config';
+//import { requestNotificationPermission } from '../firebase-config';
 
 const ReminderForm = () => {
   const [reminders, setReminders] = useState([]);
