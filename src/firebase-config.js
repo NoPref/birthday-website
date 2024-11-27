58d0074013df00484edf6a3606da795388889f74
NoPref <mrrmayvk@gmail.com>
2024-11-28 01:05:05 +0300

    fixed firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,26 +1,33 @@
-import firebase from "firebase/compact/app";
-import "firebase/compact/messaging";
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
-  
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Messaging
+const messaging = getMessaging(app);
 
 export const requestNotificationPermission = async () => {
   try {
-    await Notification.requestPermission();
-    const token = await messaging.getToken();
-    console.log('Notification Token:', token);
-    return token;
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const token = await getToken(messaging, { vapidKey: process.env.VAPID_PUBLIC_KEY });
+      console.log("Notification Token:", token);
+      return token;
+    } else {
+      console.error("Notification permission denied");
+    }
   } catch (error) {
-    console.error('Error getting notification permission:', error);
+    console.error("Error getting notification permission:", error);
   }
 };
